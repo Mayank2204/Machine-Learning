d6d63c10df63e8fd82462c082e5f90126a1ba75d
Mayank Gurjar <mayankgurjar22@gmail.com>
2019-06-21 21:56:01 +0530

    Polynomial Regression
    Predicting the salary for diferent position using old dataset.

--- Part 2 - Regression/Section 6 - Polynomial Regression/polynomial_regression.R
+++ Part 2 - Regression/Section 6 - Polynomial Regression/polynomial_regression.R
@@ -28,12 +28,21 @@ ggplot()+
 
 #Visualizing the polynomial Regression results
 # library(ggplot2)
+x_grid = seq(min(dataset$Level), max(dataset$Level), 0.1)
 ggplot()+
-  geom_point(aes(x=dataset$Level, y=dataset$Salary),
+  geom_point(aes(x = dataset$Level, y = dataset$Salary),
              colour='red')+
-  geom_line(aes(x=dataset$Level, y=predict(poly_reg, newdata = dataset)),
+  geom_line(aes(x = x_grid, y=predict(poly_reg, newdata = data.frame(Level = x_grid))),
             colour='blue')+
   ggtitle('Truth vs Bluff (Polynomial regression)')+
   xlab('Level')+
   ylab('Salary')
-  
+
+#predictiong a new result set with linear regression
+y_pred = predict(lin_reg, data.frame(Level = 6.5))
+
+#prdicting a new result set with polynomial regression
+y_pred = predict(poly_reg, data.frame(Level = 6.5,
+                                      Level2 = 6.5^2,
+                                      Level3 = 6.5^3,
+                                      Level4 = 6.5^4))
